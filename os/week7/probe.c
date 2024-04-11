2b499bf7a44808f964d2c140cc395f4de930d020
Crescent Rose <102819224+Cresc3ntRose@users.noreply.github.com>
2024-04-11 11:50:57 +0800

    Add files via upload

--- os/week7/probe.c
+++ os/week7/probe.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
+
+// #define __LINUX
+
+#ifdef __LINUX  // 进入 Linux 系统
+    #include <stdlib.h>
+    #include <string.h>
+    #include <dirent.h>
 
 int main(int argc, char const *argv[])
 {
@@ -58,3 +62,52 @@ release:
 
     return 0;
 }
+
+#else // 进入 Windows 系统
+    #include <windows.h>
+
+int main()
+{
+    HANDLE hProcess = GetCurrentProcess();
+    MEMORY_BASIC_INFORMATION mbi;
+    DWORD_PTR addr = 0;
+
+    DWORD_PTR segments[5] = {0};
+    char* segment_names[5] = {"text", "data", "BSS", "heap", "stack"};
+    int segment_index = 0;
+
+    while (VirtualQuery((LPCVOID)addr, &mbi, sizeof(mbi)) != 0)
+    {
+        if (mbi.State == MEM_COMMIT && mbi.Type == MEM_PRIVATE)
+            if (mbi.Protect & PAGE_EXECUTE_READ)
+                segments[segment_index++] = (DWORD_PTR)mbi.BaseAddress;
+            else if (mbi.Protect & PAGE_READWRITE) 
+                if (addr == (DWORD_PTR)mbi.BaseAddress)
+                    segments[segment_index++] = (DWORD_PTR)mbi.BaseAddress;
+                else
+                    segments[segment_index++] = (DWORD_PTR)mbi.BaseAddress;
+            else if (mbi.Protect & PAGE_READONLY)
+                segments[segment_index++] = (DWORD_PTR)mbi.BaseAddress;
+        addr += mbi.RegionSize;
+    }
+
+    printf("In a Windows system, output the relative positions of each segment from low address to high address sequentially:\n");
+    for (int i = 0; i < 5; i ++ )
+        for (int j = 0; j < 5; j ++ )
+        {
+            if (segments[i] == 0)
+                break;
+            if (segments[j] == 0)
+                continue;
+            if (segments[i] == segments[j])
+            {
+                printf("\t%s\n", segment_names[j]);
+                segments[j] = 0;
+                break;
+            }
+        }
+
+    return 0;
+}
+
+#endif
